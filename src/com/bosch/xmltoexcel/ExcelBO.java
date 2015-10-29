92a0a002e984aba8f29134b15eb9e193bec0dc6d
SOUNDHARA PAVANKUMAR <pavantiruppur@gmail.com>
2015-10-29 15:23:02 +0530

    Modifier Count changes

--- src/com/bosch/xmltoexcel/ExcelBO.java
+++ src/com/bosch/xmltoexcel/ExcelBO.java
@@ -3,12 +3,15 @@ package com.bosch.xmltoexcel;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class ExcelBO {
 
 	private Set<Sheet1BO> sheet1 = new HashSet<Sheet1BO>();
 	private List<Sheet2BO> sheet2 = new ArrayList<Sheet2BO>();
+	private Map<String, Map<String, Integer>> ownedModifierMap;
+	private Map<String, Map<String, Integer>> impModifierMap;
 	
 	public Set<Sheet1BO> getSheet1() {
 		return sheet1;
@@ -21,5 +24,18 @@ public class ExcelBO {
 	}
 	public void setSheet2(List<Sheet2BO> sheet2) {
 		this.sheet2 = sheet2;
+	}
+	public Map<String, Map<String, Integer>> getOwnedModifierMap() {
+		return ownedModifierMap;
+	}
+	public void setOwnedModifierMap(
+			Map<String, Map<String, Integer>> ownedModifierMap) {
+		this.ownedModifierMap = ownedModifierMap;
+	}
+	public Map<String, Map<String, Integer>> getImpModifierMap() {
+		return impModifierMap;
+	}
+	public void setImpModifierMap(Map<String, Map<String, Integer>> impModifierMap) {
+		this.impModifierMap = impModifierMap;
 	}
 }
